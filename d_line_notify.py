0f0feb0b39d9657ac5bffa417fbf6b996289a8a5
dtakeichi <57287387+dtakeichi@users.noreply.github.com>
2022-03-05 16:04:10 +0900

    Update d_line_notify.py

--- d_line_notify.py
+++ d_line_notify.py
@@ -1,1 +1,23 @@
+# LINE通知の設定を行うためのファイル
+import requests
 
+LINE_NOTIFY_TOKEN = "?????????????????????"
+
+
+"""
+使い方
+(1) インスタンス化 line_notify = LineNotify()
+(2) 贈りたいメッセージをHelloとすると line_notify.send("Hello")
+"""
+
+class LineNotify:
+    def __init__(self):
+        self.line_notify_token = LINE_NOTIFY_TOKEN
+        self.line_notify_api = "https://notify-api.line.me/api/notify"
+        self.headers = {
+          "Authorization": f"Bearer {self.line_notify_token}"
+        }
+
+    def send(self, msg):
+        msg = { "message": f" {msg}" }
+        requests.post(self.line_notify_api, headers = self.headers, data = msg)
