d7e88e777881144058ca8f151f6945a281b47db1
dtakeichi <57287387+dtakeichi@users.noreply.github.com>
2022-03-06 11:21:36 +0900

    Update d_bot.py

--- d_bot.py
+++ d_bot.py
@@ -1,44 +1,59 @@
+
 import pandas as pd
-pd.options.display.float_format = '{:.2f}'.format #小数点以下2桁まで表示
-from pprint import pprint
-import ccxt
 import requests
 import time
 import sys
 import traceback
 from datetime import datetime
+import talib
+import ccxt # pip install ccxt==1.45.18  バージョン注意
+from pprint import pprint
+import requests
 
-# 以下同じディレクトリの関数からimport
-import d_functions
-import d_functions2
-from d_line_notify import LineNotify
+from functions import get_ohlcv
+from functions2 import long_entry
+from functions2 import long_close
+from strategy import calc_entry_signal
+from line_notify import LineNotify
 
-##################################開始
 
-# インスタンス化
+"""
+## 準備 ##
+"""
 bybit = ccxt.bybit(
             {
-                "apiKey": "?????",
-                "secret": "?????",
+                "apiKey": "???",
+                "secret": "???????",
                 "urls": {
                     "api": "https://api-testnet.bybit.com/" # testnet（デモ版）を使用する場合はこの記述が必要
                 }
             }
         )
-
 symbol = "BTC/USDT" 
-amount = 0.0001
+amount = 0.01
 
-# Lineの設定
-line_notify = LineNotify()
-line_notify.send("Start trading")
+LINE_NOTIFY_TOKEN = "???"
+line_notify = LineNotify(token = LINE_NOTIFY_TOKEN)
 
+
+"""
+## 実行部分 ##
+"""
 
 while 1:
-    entry_signal = calc_entry_signal()
+    df = get_ohlcv(limit=1)
+    entry_signal = calc_entry_signal(df)
     
     if entry_signal == True:
         long_entry(bybit)
         entry_price = get_ohlcv(limit=1)['close'][199] #参入時の価格を保持
-        time.sleep(300)
-        long_close(bybit) # 5分後にポジションは自動で解消することにする
+        line_notify.send(f'entry at {entry_price}')
+        time.sleep(100)
+        while 1:
+            now_price = get_ohlcv(limit=1)['low'][199]
+            entry_price * 1.01 < now_price
+            long_close(bybit) # 5分後にポジションは自動で解消することにする
+            line_notify.send(f'close at {now_price}')
+            break
+            
+    time.sleep(60)
