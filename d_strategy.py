37a9cdb898f123a2680f462beac40e1cc254b426
dtakeichi <57287387+dtakeichi@users.noreply.github.com>
2022-03-12 20:52:30 +0900

    Update d_strategy.py

--- d_strategy.py
+++ d_strategy.py
@@ -12,3 +12,8 @@ def calc_entry_signal(df):
     rule2 = df['close'].diff(1)[197] > 0 and df['close'].diff(1)[196] < 0 #and df['close'].diff(1)[196] < 0
     
     return rule1 # and rule2  条件(2)をなかなか満たさないので and以降コメントアウト
+
+def calc_close_signal(df, entry_price, best_ask):
+    rule1 = entry_price * 1.005 < best_ask
+    rule2 = df['close'][199] < entry_price * 0.9 # loss cut
+    return rule1 or rule2
