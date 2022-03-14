b37acce4b3dffcbfe0f2631578849fd775bafd72
dtakeichi <57287387+dtakeichi@users.noreply.github.com>
2022-03-14 23:59:25 +0900

    Update d_strategy.py

--- d_strategy.py
+++ d_strategy.py
@@ -1,19 +1,18 @@
 """
 仮説
-# 動きの前は取引量が増加する
-(1) 過去15分の平均取引量 < 最新3分の取引量 
-(2) 過去2分連続して下がっていていま上がり出したときに参入、谷を特定する感じ lagがあったので1つ前のデータで判定
 """
 
 def calc_entry_signal(df):
-    volume_mean = df[-15:]['volume'].mean() # 過去15分の取引量の平均
-    volume_std = df[-15:]['volume'].std() # 過去15分の取引量の標準偏差
-    rule1 = df[-3:]['volume'].mean() > volume_mean 
-    rule2 = df['close'].diff(1)[197] > 0 and df['close'].diff(1)[196] < 0 #and df['close'].diff(1)[196] < 0
-    
-    return rule1 # and rule2  条件(2)をなかなか満たさないので and以降コメントアウト
+    rule1 = df[-3:]['volume'].mean() > df[-15:]['volume'].mean()
+    rule2 = df['close'].diff(1)[199] > 0 and df['close'].diff(1)[198] < 0 #and df['close'].diff(1)[196] < 0
+    mean = (df['high'].max() +df['low'].min())/2
+    rule3 = df['close'][199] < mean
 
-def calc_close_signal(df, entry_price, best_ask):
-    rule1 = entry_price * 1.005 < best_ask
-    rule2 = df['close'][199] < entry_price * 0.9 # loss cut
-    return rule1 or rule2
+    return rule1 and rule2 and rule3 
+
+def calc_close_signal(df, price, ask):
+    rule1 = price + 100 < ask  #rule1 = price * 1.002 < ask
+    rule2 = df['close'][199] < price - 100 # price * 0.99 # loss cut
+    rule3 = df['close'].diff(1)[199] < 0 and df['close'].diff(1)[198] > 0
+
+    return (rule1 and rule3) or rule2
